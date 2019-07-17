0126136c4245c3ad8144b32e8063f956f86b687d
Greger Wedel <greger.wedel@cxense.com>
2019-07-17 08:15:19 +0200

    Fix ios build with latest xcode beta

---
+++ ios/Runner/AppDelegate.m
@@ -0,0 +1,18 @@
+#include "AppDelegate.h"
+#import "FlutterAuth0Plugin.h"
+#include "GeneratedPluginRegistrant.h"
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application
+    didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+  [GeneratedPluginRegistrant registerWithRegistry:self];
+  // Override point for customization after application launch.
+  return [super application:application didFinishLaunchingWithOptions:launchOptions];
+}
+
+- (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options {
+    return [FlutterAuth0Plugin application:app openURL:url options:options];
+}
+
+@end
