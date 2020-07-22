bdc95fa16f8b9e3771b87c2ecc43ccaf4fb8843d
Greger Wedel <greger@greger.io>
2020-07-22 09:55:30 +0200

    Remove unused io lib

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-import 'dart:io' show Platform;
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
